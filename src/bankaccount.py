0c5c0f6ab96b5fb67766dbd526531d31eec3d307
Anna Wester <annawester73@gmail.com>
2026-03-23 08:55:53 +0100

    Commit with bankaccount code and TDD green

--- src/bankaccount.py
+++ src/bankaccount.py
@@ -1,2 +1,17 @@
 class BankAccount:
-    pass
+    def __init__(self, balance, logger):
+        self.balance = balance
+        self.logger = logger
+
+    def deposit(self, amount):
+        self.balance += amount
+        self.logger.log(f"Insättning {amount}kr. Nytt saldo {self.balance}kr")
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            self.logger.log(f"Uttag {amount}kr. Nytt saldo {self.balance}kr")
+            return True
+        else:
+            self.logger.log(f"Du har inte tillräckligt på kontot. Saldo {self.balance}kr")
+            return False
