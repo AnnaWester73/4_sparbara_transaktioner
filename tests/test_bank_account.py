9e43af81de0358e1118fe0f740ae20aa110761fe
Anna Wester <annawester73@gmail.com>
2026-03-23 08:50:07 +0100

    Commit För Adding falied testcase för BankAccount

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,5 +1,5 @@
 # Testfall 1
-# Saldo 0kr
+# Saldo 50 kr
 # Insättning 100kr
 
 # Testfall 2
@@ -14,4 +14,46 @@
 # return False
 # Loggning: Finns inte tillräcklig på kontot
 
+from src.bankaccount import BankAccount
+
+class LoggerSpy:
+    def __init__(self):
+        self.messages = []
+
+    def log(self, message):
+        self.messages.append(message)
+
+def test_deposit_increase_balance_and_logs ():
+
+    logger = LoggerSpy()
+    account = BankAccount(50, logger)
+
+    account.deposit(100)
+
+    assert account.balance == 150
+    assert logger.messages == ["Insättning 100kr. Nytt saldo 150kr"]
+
+
+def test_withdraw_success_reduces_balance_and_logs():
+
+    logger = LoggerSpy()
+    account = BankAccount(100, logger)
+
+    result = account.withdraw(20)
+
+    assert result is True
+    assert account.balance == 80
+    assert logger.messages == ["Uttag 20kr. Nytt saldo 80kr"]
+
+
+def test_withdraw_fail_and_logs():
+
+    logger = LoggerSpy()
+    account = BankAccount(100, logger)
+
+    result = account.withdraw(150)
+
+    assert result is False
+    assert account.balance == 100
+    assert logger.messages == ["Du har inte tillräckligt på kontot. Saldo 100kr"]
 
