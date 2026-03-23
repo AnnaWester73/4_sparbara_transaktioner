b80dadea510989296d17587d1f4e9745ff4c2737
Anna Wester <annawester73@gmail.com>
2026-03-23 08:04:13 +0100

    Commit för vilka testfall som ska skrivas

---
+++ tests/test_bank_account.py
@@ -0,0 +1,17 @@
+# Testfall 1
+# Saldo 0kr
+# Insättning 100kr
+
+# Testfall 2
+# Saldo 100kr
+# Uttag 20kr
+# Return True
+# Nytt saldo 80kr
+
+# Testfall 3
+# Saldo 100kr
+# Uttag 150kr
+# return False
+# Loggning: Finns inte tillräcklig på kontot
+
+
---
+++ tests/test_transaction.py
@@ -0,0 +1,12 @@
+# Test 1
+# Saldo konto A 100kr
+# Saldo konto B 50kr
+# Överför 20kr från Konto A till konto B
+# Nytt saldo konto A 80kr
+# Nytt saldo konto B 70kr
+
+# Test 2
+# Saldo konto A 100kr
+# Saldo konto B 50kr
+# Överför 120kr från Konto A till konto B
+# Överföring kan inte genomföras mellan konton
