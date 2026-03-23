8a7eca4e818a9a11a8aa3cce1f188706f5b2363f
Anna Wester <annawester73@gmail.com>
2026-03-23 09:04:21 +0100

    Commit with failed TDD for test_transaction.py

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,12 +1,47 @@
 # Test 1
-# Saldo konto A 100kr
-# Saldo konto B 50kr
+# Saldo konto 1 100kr
+# Saldo konto 2 50kr
 # Överför 20kr från Konto A till konto B
 # Nytt saldo konto A 80kr
 # Nytt saldo konto B 70kr
 
 # Test 2
-# Saldo konto A 100kr
-# Saldo konto B 50kr
-# Överför 120kr från Konto A till konto B
+# Saldo konto 1 100kr
+# Saldo konto 2 50kr
+# Överför 120kr från Konto 1 till konto 2
 # Överföring kan inte genomföras mellan konton
+
+from src.bank_account import BankAccount
+from src.transaction import Transaction
+
+class LoggerSpy:
+    def __init__(self):
+        self.messages = []
+
+    def log(self, message):
+        self.messages.append(message)
+
+def test_transfer_success_move_money_and_logs():
+    logger = LoggerSpy()
+    account1 = BankAccount(100, logger)
+    account2 = BankAccount(50, logger)
+
+    Transaction.transfer(20, account1, account2)
+
+    assert account1.balance == 80
+    assert account2.balance == 70
+
+    assert logger.messages == ["Konto 1: Uttag 20kr. Nytt saldo 80kr", "Konto 2: Insättning 20kr. Nytt saldo 70kr"]
+
+
+def test_transfer_fail_move_money_and_logs():
+    logger = LoggerSpy()
+    account1 = BankAccount(100, logger)
+    account2 = BankAccount(50, logger)
+
+    Transaction.transfer(120, account1, account2)
+
+    assert account1.balance == 100
+    assert account2.balance == 50
+
+    assert logger.messages == ["Du har inte tillräckligt på kontot. Saldo konto 1 100kr"]
